9993ea6e1f21a5a118fbe7b490adda7bdc3ffc3c
Johnny Chiu <johnnychiu123@gmail.com>
2017-07-14 10:52:06 -0400

    d

--- Speed Study/QuestionViewController.swift
+++ Speed Study/QuestionViewController.swift
@@ -9,17 +9,22 @@ import UIKit
 import Foundation
 
 class QuestionViewController: UIViewController{
-    
-    var num: Int = 0
+    var answerList = [String]()
     var question: Question!
+    var verify: String = " "
+    var randInt: Int = 0
     @IBOutlet var buttonSet: [UIButton]!
     
     @IBAction func buttonSetTapped(sender: UIButton)
     {
-        //for button in buttonSet {
-        
-            loadQuestion()
-        //}
+        for button in buttonSet {
+            let index = buttonSet.index(of: sender)!
+            if randInt == index
+            {
+                Score.score += 1
+                loadQuestion()
+            }
+        }
     }
     
     @IBOutlet weak var questionNumber: UILabel!
@@ -30,12 +35,32 @@ class QuestionViewController: UIViewController{
         
     }
     func loadQuestion(){
-        question = QuestionGenerator.generatorQuestion(num: num)
-        num += 1
-        questionNumber.text = "Hello" //question.text
-        buttonSet[2].setTitle(question.answer, for: .normal)
-        buttonSet[0].setTitle(QNA.mathAnswers[Int(arc4random_uniform(UInt32(question.questionNum)))], for: .normal)
-        buttonSet[1].setTitle(QNA.mathAnswers[Int(arc4random_uniform(UInt32(question.questionNum)))], for: .normal)
-        buttonSet[3].setTitle(QNA.mathAnswers[Int(arc4random_uniform(UInt32(question.questionNum)))], for: .normal)
+        randInt = Int(arc4random_uniform(UInt32(3)))
+        question = QuestionGenerator.generatorQuestion(num: randInt)
+        questionNumber.text = question.text //question.text
+        
+        randInt = Int(arc4random_uniform(UInt32(3)))
+        switch randInt{
+        case 0:
+            buttonSet[0].setTitle(question.answer, for: .normal)
+        case 1:
+            buttonSet[1].setTitle(question.answer, for: .normal)
+        case 2:
+            buttonSet[2].setTitle(question.answer, for: .normal)
+        case 3:
+            buttonSet[3].setTitle(question.answer, for: .normal)
+        default:
+            fatalError("index out of bounds")
+        }
+        
+//        randInt = Int(arc4random_uniform(UInt32(question.questionNum)))
+//        buttonSet[0].setTitle(QNA.mathAnswers[randInt], for: .normal)
+//        
+//        randInt = Int(arc4random_uniform(UInt32(question.questionNum)))
+//        buttonSet[1].setTitle(QNA.mathAnswers[randInt], for: .normal)
+//        
+//        buttonSet[2].setTitle(question.answer, for: .normal)
+//        
+//        buttonSet[3].setTitle(QNA.mathAnswers[Int(arc4random_uniform(UInt32(question.questionNum)))], for: .normal)
     }
 }
